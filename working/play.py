0d5d4701c928324376c3bd3a46539e87e47d4f4c
Kevin Mathew T <mathewtkevin@gmail.com>
2020-12-19 21:51:13 +0530

    update for colab

--- working/play.py
+++ working/play.py
@@ -25,7 +25,24 @@
 import timm
 import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
-list = timm.list_models()
+# pp = pprint.PrettyPrinter(indent=4)
+# list = timm.list_models()
 
-pp.pprint(list)
+# pp.pprint(list)
+
+from prettytable import PrettyTable
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: continue
+        param = parameter.numel()
+        table.add_row([name, param])
+        total_params+=param
+    # print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+
+model = timm.create_model("densenet121")
+count_parameters(model)
