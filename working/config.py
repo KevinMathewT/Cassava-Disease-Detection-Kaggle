4b18573e5a6fa6071ea702d76f122d9776e85350
Kevin Mathew T <mathewtkevin@gmail.com>
2020-12-20 15:10:34 +0530

    after the damage

--- working/config.py
+++ working/config.py
@@ -31,7 +31,7 @@ MAX_EPOCHS           = 10
 
 N_CLASSES            = 5
 
-TRAIN_BATCH_SIZE     = 16
+TRAIN_BATCH_SIZE     = 32
 VALID_BATCH_SIZE     = 32
 
 # NET                  = 'SEResNeXt50_32x4d'
