73cb55f011796b15ebc27a6720b59d75026bfb94
Kevin Mathew T <mathewtkevin@gmail.com>
2020-12-21 16:26:00 +0530

    kaggle commit

--- working/config.py
+++ working/config.py
@@ -2,8 +2,8 @@ import os
 from .utils import seed_everything
 
 # INPUT_PATH           = "./input"  # PC
-INPUT_PATH           = "../input" # Kaggle
-# INPUT_PATH           = "."        # Colab
+# INPUT_PATH           = "../input" # Kaggle
+INPUT_PATH           = "."        # Colab
 GENERATED_FILES_PATH = "./generated/"
 DATASET_PATH         = os.path.join(INPUT_PATH, "cassava-leaf-disease-classification/")
 TRAIN                = os.path.join(DATASET_PATH, "train.csv")
