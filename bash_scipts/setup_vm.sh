286dca79e2a0f0f044358c1906c1a36da74128e6
Kevin Mathew T <mathewtkevin@gmail.com>
2020-12-23 19:10:26 +0530

    edit for ec2

--- bash_scipts/setup_vm.sh
+++ bash_scipts/setup_vm.sh
@@ -5,7 +5,7 @@ sudo file -s /dev/xvdf
 sudo mkdir /vol
 sudo mount /dev/xvdf /vol
 sudo df -hT
-sudo cp -a /vol/. ./input/
+sudo rsync -ah --progress /vol/. ./input/
 git init
 git remote add origin https://KevinMathewT:skevin943@github.com/KevinMathewT/Cassava-Disease-Detection.git
 git pull origin master
