c63e1dd2ae7820e89eb242594ce840b9289c53ae
Kevin Mathew T <mathewtkevin@gmail.com>
2020-12-28 20:13:47 +0530

    add for kaggle

--- bash_scipts/setup_vm.sh
+++ bash_scipts/setup_vm.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 source activate pytorch_latest_p37
-lsblk
-sudo file -s /dev/xvdf
-sudo mkdir /vol
-sudo mount /dev/xvdf /vol
-sudo df -hT
-sudo rsync -ah --progress /vol/. ./input/
 git init
 git remote add origin https://KevinMathewT:skevin943@github.com/KevinMathewT/Cassava-Disease-Detection.git
 git pull origin master
@@ -17,6 +11,12 @@ pip install --upgrade timm
 pip install adabelief_pytorch
 pip install ranger_adabelief
 pip install albumentations
+lsblk
+sudo file -s /dev/xvdf
+sudo mkdir /vol
+sudo mount /dev/xvdf /vol
+sudo df -hT
+sudo rsync -ah --progress /vol/. ./input/
 python -m working.create_folds
 git pull origin master
 python -m working.train
