c59bc71a3477dd9065ccd7366b5ca9ce462c7d6f
KevinMathewT <mathewtkevin@gmail.com>
2021-01-06 14:01:26 +0530

    for kaggle

--- src/config.py
+++ src/config.py
@@ -34,8 +34,8 @@ MAX_EPOCHS            = 15
 
 N_CLASSES             = 5
 
-TRAIN_BATCH_SIZE      = 16
-VALID_BATCH_SIZE      = 16
+TRAIN_BATCH_SIZE      = 32
+VALID_BATCH_SIZE      = 32
 ACCUMULATE_ITERATION  = 2
 
 NET                   = 'SEResNeXt50_32x4d_BH'
