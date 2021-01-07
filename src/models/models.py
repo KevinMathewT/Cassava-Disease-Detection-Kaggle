e1e2f7a237a25309cf8fce0e80a3bf9efd9f919e
KevinMathewT <mathewtkevin@gmail.com>
2021-01-07 19:33:56 +0530

    edit for kaggle

--- src/models/models.py
+++ src/models/models.py
@@ -33,28 +33,23 @@ class SEResNeXt50_32x4d_BH(nn.Module):
     def __init__(self, pretrained=False):
         super().__init__()
         self.model_arch = "seresnext50_32x4d"
-        self.model = timm.create_model(self.model_arch, pretrained=pretrained)
-        model_list = list(self.model.children())
-        model_list[-1] = nn.Identity()
-        model_list[-2] = nn.Identity()
-        self.net = nn.Sequential(*model_list)
+        self.net = nn.Sequential(*list(
+            timm.create_model(self.model_arch, pretrained=pretrained).children())[:-2])
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fea_bn = nn.BatchNorm1d(2048)
         self.fea_bn.bias.requires_grad_(False)
         self.binary_head = BinaryHead(N_CLASSES, emb_size=2048, s=1)
         self.dropout = nn.Dropout(p=0.2)
-        self.fc = nn.Linear(in_features=2048, out_features=N_CLASSES)
 
     def forward(self, x):
-        x = self.net(x)
-        x = self.avg_pool(x)
-        x = x.view(x.size(0), -1)
-        # x = self.fea_bn(x)
+        img_feature = self.net(x)
+        img_feature = self.avg_pool(img_feature)
+        img_feature = img_feature.view(img_feature.size(0), -1)
+        fea = self.fea_bn(img_feature)
         # fea = self.dropout(fea)
-        x = self.binary_head(x)
-        # x = self.fc(x)
+        output = self.binary_head(fea)
 
-        return x
+        return output
 
 
 class ResNeXt50_32x4d_BH(nn.Module):
