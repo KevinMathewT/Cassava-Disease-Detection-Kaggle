4e13d0336d89ebd586a8c7d5fc720218f20094f0
KevinMathewT <mathewtkevin@gmail.com>
2021-01-07 19:20:21 +0530

    edit for kaggle

--- src/config.py
+++ src/config.py
@@ -22,7 +22,7 @@ PARALLEL_FOLD_TRAIN   = False
 SEED                  = 719
 FOLDS                 = 5
 SEEDS                 = 1
-MIXED_PRECISION_TRAIN = True
+MIXED_PRECISION_TRAIN = False
 
 H                     = 512
 W                     = 512
