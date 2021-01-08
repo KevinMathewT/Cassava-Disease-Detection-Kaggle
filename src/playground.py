afcf98c7524d38a8dd2be75b016046b162ba07e3
KevinMathewT <mathewtkevin@gmail.com>
2021-01-08 18:03:01 +0530

    edit for kaggle

--- src/playground.py
+++ src/playground.py
@@ -7,24 +7,17 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 
-from src.dataset import CassavaDataset
+from src.dataset import CassavaDataset, get_infer_dataloader
 from src.engine import get_device, get_net, get_optimizer_and_scheduler, train_one_epoch, valid_one_epoch
 from src.config import *
 from src.utils import *
 from src.loss import FocalCosineLoss, SmoothCrossEntropyLoss, bi_tempered_logistic_loss
 
-from IPython.display import FileLinks
-FileLinks(WEIGHTS_PATH)
+# from IPython.display import FileLinks
+# FileLinks(WEIGHTS_PATH)
 
 df = pd.read_csv(TRAIN_FOLDS)
-dataset = CassavaDataset(df=df,
-                         data_root=TRAIN_IMAGES_DIR,
-                         transforms=get_valid_transforms())
-dataloader = DataLoader(dataset,
-                        batch_size=3,
-                        drop_last=False,
-                        num_workers=0,
-                        shuffle=False)
+dataloader = get_infer_dataloader(infer=df)
 device = get_device(n=0)
 net = get_net(name=NET, pretrained=False)
 net.load_state_dict(torch.load("../input/model-weights/SEResNeXt50_32x4d_BH_fold_2_11.bin"))
