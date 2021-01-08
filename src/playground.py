c9369e228b42158626b65da88b246eca9920090c
KevinMathewT <mathewtkevin@gmail.com>
2021-01-08 18:29:58 +0530

    edit for kaggle

--- src/playground.py
+++ src/playground.py
@@ -16,27 +16,17 @@ from src.loss import FocalCosineLoss, SmoothCrossEntropyLoss, bi_tempered_logist
 # from IPython.display import FileLinks
 # FileLinks(WEIGHTS_PATH)
 
-# df = pd.read_csv(TRAIN_FOLDS)
-# print("Here")
-# dataloader = get_infer_dataloader(infer=df)
-# print("Here")
-# device = get_device(n=0)
-# print("Here")
+df = pd.read_csv(TRAIN_FOLDS)
+dataloader = get_infer_dataloader(infer=df)
+device = get_device(n=0)
 net = get_net(name=NET, pretrained=False)
-# print("Here")
-# net = net.to(device)
-# print("Here")
-# if USE_TPU:
-#     import torch_xla.utils.serialization as xser
-#     net.load_state_dict(xser.load("../input/model-weights/SEResNeXt50_32x4d_BH_fold_2_11.bin"))
-# else:
 net.load_state_dict(torch.load("../input/model-weights/SEResNeXt50_32x4d_BH_fold_2_11.bin", map_location=torch.device('cpu')))
-# print("Here")
+net = net.to(device)
 
-# preds = np.empty((0, 5), dtype=np.float64)
-# for images, labels in tqdm(dataloader):
-#     images, labels = images.to(device), labels.to(device)
-#     predictions = net(images).cpu().numpy()
-#     preds = np.concatenate([preds, predictions], axis=0)
+preds = np.empty((0, 5), dtype=np.float64)
+for images, labels in tqdm(dataloader):
+    images, labels = images.to(device), labels.to(device)
+    predictions = net(images).cpu().numpy()
+    preds = np.concatenate([preds, predictions], axis=0)
     
-# print(preds.shape)
+print(preds.shape)
