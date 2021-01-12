d9296fac3c3e962e22f2dc92e79dc59dd5dc2218
KevinMathewT <mathewtkevin@gmail.com>
2021-01-12 23:26:45 +0530

    edit for kaggle

--- src/config.py
+++ src/config.py
@@ -47,8 +47,8 @@ SCHEDULER_BATCH_STEP  = True
 
 N_CLASSES             = 5
 
-TRAIN_BATCH_SIZE      = 16
-VALID_BATCH_SIZE      = 16
+TRAIN_BATCH_SIZE      = 32
+VALID_BATCH_SIZE      = 32
 ACCUMULATE_ITERATION  = 2
 
 NET                   = "ViTBase16" # [SEResNeXt50_32x4d_BH, ResNeXt50_32x4d_BH, ViTBase16_BH, ViTBase16
