ccf1bdff6301c2829304d812b2a07631d7f3db90
KevinMathewT <mathewtkevin@gmail.com>
2021-01-12 23:23:23 +0530

    edit for kaggle

--- src/config.py
+++ src/config.py
@@ -51,7 +51,7 @@ TRAIN_BATCH_SIZE      = 2 * 8
 VALID_BATCH_SIZE      = 2 * 8
 ACCUMULATE_ITERATION  = 2
 
-NET                   = "seresnext50_32x4d" # [SEResNeXt50_32x4d_BH, ResNeXt50_32x4d_BH, ViTBase16_BH, ViTBase16
+NET                   = "ViTBase16" # [SEResNeXt50_32x4d_BH, ResNeXt50_32x4d_BH, ViTBase16_BH, ViTBase16
                                              #  resnext50_32x4d, seresnext50_32x4d, tf_efficientnet_b4_ns, ['vit_base_patch16_224', 'vit_base_patch16_384', 'vit_base_patch32_384', 'vit_base_resnet26d_224', 'vit_base_resnet50d_224', 'vit_huge_patch16_224', 'vit_huge_patch32_384', 'vit_large_patch16_224', 'vit_large_patch16_384', 'vit_large_patch32_384', 'vit_small_patch16_224', 'vit_small_resnet26d_224', 'vit_small_resnet50d_s3_224']
 
 PRETRAINED            = True
